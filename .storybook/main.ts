f8dd7adb0b9d23e4781731fc8775a0fd1498dd7a
Yusuke Fujiki <yusuke@fujikkys.com>
2022-02-01 17:32:31 +0900

    :lipstick: add storybook settings

---
+++ .storybook/main.ts
@@ -0,0 +1,30 @@
+import { StorybookConfig } from "@storybook/react/types";
+import { TsconfigPathsPlugin } from "tsconfig-paths-webpack-plugin";
+
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack5",
+  },
+  staticDirs: ["../public"],
+  webpackFinal: (config) => ({
+    ...config,
+    module: {
+      ...config.module,
+      rules: [
+        ...(config.module?.rules ?? []),
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: [{ loader: "@linaria/webpack-loader" }],
+        },
+      ],
+    },
+    resolve: {
+      ...config.resolve,
+      plugins: [...(config.resolve?.plugins ?? []), new TsconfigPathsPlugin()],
+    },
+  }),
+} as StorybookConfig;
