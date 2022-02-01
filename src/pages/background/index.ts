e9332e1cffad827fd18f83e8c81e6706a38c365a
Yusuke Fujiki <yusuke@fujikkys.com>
2022-02-01 17:34:25 +0900

    :necktie: add background script

---
+++ src/pages/background/index.ts
@@ -0,0 +1,78 @@
+import { RuntimeMessageRequest, TabMessageRequest } from "~/types";
+
+import { createLike, deleteLike, getLikeInfo } from "./notionApi";
+
+chrome.runtime.onMessage.addListener(
+  async (request: RuntimeMessageRequest, sender) => {
+    const tabId = sender.tab?.id;
+    if (typeof tabId !== "number") return;
+
+    switch (request.message) {
+      case "init":
+        {
+          await chrome.storage.local.set({ userId: request.userId });
+
+          const result = await getLikeInfo(request.url, request.userId);
+          if (result) {
+            chrome.tabs.sendMessage<TabMessageRequest>(tabId, {
+              message: "showLikeButton",
+              ...result,
+            });
+          } else {
+            chrome.tabs.sendMessage<TabMessageRequest>(tabId, {
+              message: "hideLikeButton",
+            });
+          }
+        }
+        break;
+      case "createLike":
+        try {
+          const result = await createLike(request.url, request.userId);
+          chrome.tabs.sendMessage<TabMessageRequest>(tabId, {
+            message: "updateLikeButton",
+            ...result,
+          });
+        } catch (e) {
+          chrome.tabs.sendMessage<TabMessageRequest>(tabId, {
+            message: "revertLikeButton",
+          });
+        }
+        break;
+      case "deleteLike":
+        try {
+          const result = await deleteLike(request.url, request.userId);
+          chrome.tabs.sendMessage<TabMessageRequest>(tabId, {
+            message: "updateLikeButton",
+            ...result,
+          });
+        } catch (e) {
+          chrome.tabs.sendMessage<TabMessageRequest>(tabId, {
+            message: "revertLikeButton",
+          });
+        }
+        break;
+      default:
+        break;
+    }
+  }
+);
+
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo) => {
+  const url = changeInfo.url;
+  if (!url) return;
+
+  chrome.tabs.sendMessage<TabMessageRequest>(tabId, {
+    message: "hideLikeButton",
+  });
+
+  const { userId } = await chrome.storage.local.get(["userId"]);
+  if (typeof userId !== "string") return;
+
+  const result = await getLikeInfo(url, userId);
+  if (!result) return;
+
+  chrome.tabs.sendMessage<TabMessageRequest>(tabId, {
+    message: "showLikeButton",
+    ...result,
+  });
+});
---
+++ src/pages/background/notionApi.ts
@@ -0,0 +1,97 @@
+import { Client } from "@notionhq/client";
+
+import { SettingsValues } from "~/types";
+
+type Page = Awaited<ReturnType<typeof Client.prototype.pages.retrieve>>;
+
+const DEFAULT_LIKE_PROP = "Like";
+
+const getNotion = async (url: string) => {
+  const u = new URL(url);
+  if (u.host !== "www.notion.so") return null;
+
+  const queryPageId = u.searchParams.get("p");
+  const pageId = queryPageId || u.pathname.split(/(-|\/)/g).at(-1);
+  const pageMode = queryPageId ? "popup" : "page";
+  if (!pageId) return null;
+
+  const settings = (await chrome.storage.local.get([
+    "apiToken",
+    "likeProp",
+  ])) as SettingsValues;
+  const likeProp = settings.likeProp ?? DEFAULT_LIKE_PROP;
+  if (!settings.apiToken) return null;
+
+  const client = new Client({
+    auth: settings.apiToken,
+    fetch: (input, init) => {
+      const newInit = { ...init, method: init?.method?.toUpperCase() };
+      return fetch(input, newInit);
+    },
+  });
+
+  return { pageId, pageMode, likeProp, client } as const;
+};
+
+const resolveLikedPeople = (page: Page, likeProp: string) => {
+  if (!("properties" in page)) throw Error("Could not find properties in page");
+
+  const like = page.properties[likeProp];
+  if (!like || like.type !== "people")
+    throw Error("Could not find like prop with peoople type");
+
+  return like.people;
+};
+
+const createLikeInfo = (page: Page, userId: string, likeProp: string) => {
+  const people = resolveLikedPeople(page, likeProp);
+  const isLiked = people.some((people) => people.id === userId);
+  const likeCount = people.length;
+  return { isLiked, likeCount };
+};
+
+export const getLikeInfo = async (url: string, userId: string) => {
+  const notion = await getNotion(url);
+  if (!notion) return null;
+
+  const { pageId, pageMode, likeProp, client } = notion;
+
+  try {
+    const page = await client.pages.retrieve({ page_id: pageId });
+    return { ...createLikeInfo(page, userId, likeProp), pageMode, url };
+  } catch {
+    return null;
+  }
+};
+
+export const createLike = async (url: string, userId: string) => {
+  const notion = await getNotion(url);
+  if (!notion) throw Error("Could not find valid api settings");
+
+  const { pageId, pageMode, likeProp, client } = notion;
+  const page = await client.pages.retrieve({ page_id: pageId });
+  const people = resolveLikedPeople(page, likeProp);
+  const newPeople = people.map((p) => ({ id: p.id })).concat({ id: userId });
+  const newPage = await client.pages.update({
+    page_id: pageId,
+    properties: { [likeProp]: { type: "people", people: newPeople } },
+  });
+  return { ...createLikeInfo(newPage, userId, likeProp), pageMode, url };
+};
+
+export const deleteLike = async (url: string, userId: string) => {
+  const notion = await getNotion(url);
+  if (!notion) throw Error("Could not find valid api settings");
+
+  const { pageId, pageMode, likeProp, client } = notion;
+  const page = await client.pages.retrieve({ page_id: pageId });
+  const people = resolveLikedPeople(page, likeProp);
+  const newPeople = people
+    .filter((p) => p.id !== userId)
+    .map((p) => ({ id: p.id }));
+  const newPage = await client.pages.update({
+    page_id: pageId,
+    properties: { [likeProp]: { type: "people", people: newPeople } },
+  });
+  return { ...createLikeInfo(newPage, userId, likeProp), pageMode, url };
+};
