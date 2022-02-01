6c02b8c1992babeba9235e6d488d1e7ef4d9156c
Yusuke Fujiki <yusuke@fujikkys.com>
2022-02-01 17:36:09 +0900

    :label: add shared typings

---
+++ src/types.ts
@@ -0,0 +1,43 @@
+export type RuntimeMessageRequest =
+  | {
+      readonly message: "init";
+      readonly userId: string;
+      readonly url: string;
+    }
+  | {
+      readonly message: "createLike";
+      readonly userId: string;
+      readonly url: string;
+    }
+  | {
+      readonly message: "deleteLike";
+      readonly userId: string;
+      readonly url: string;
+    };
+
+export type TabMessageRequest =
+  | {
+      readonly message: "showLikeButton";
+      readonly isLiked: boolean;
+      readonly likeCount: number;
+      readonly pageMode: "page" | "popup";
+      readonly url: string;
+    }
+  | {
+      readonly message: "updateLikeButton";
+      readonly isLiked: boolean;
+      readonly likeCount: number;
+      readonly pageMode: "page" | "popup";
+      readonly url: string;
+    }
+  | {
+      readonly message: "revertLikeButton";
+    }
+  | {
+      readonly message: "hideLikeButton";
+    };
+
+export type SettingsValues = {
+  readonly apiToken: string | undefined;
+  readonly likeProp: string | undefined;
+};
